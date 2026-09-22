6f2c8350ce17e26417870d45ae033e161b571764
Aarti Gore <aartigore2005@gmail.com>
2026-09-23 00:01:30 +0530

    Time: 8 ms (62.22%), Space: 47.1 MB (26.36%) - LeetHub

---
+++ 0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -0,0 +1,41 @@
+class Solution {
+    public String shortestPalindrome(String s) {
+
+        if (s.length() <= 1) {
+            return s;
+        }
+
+        String rev = new StringBuilder(s).reverse().toString();
+
+        // Combine original + "#" + reverse
+        String combined = s + "#" + rev;
+
+        // Build LPS array
+        int[] lps = new int[combined.length()];
+
+        for (int i = 1; i < combined.length(); i++) {
+            int j = lps[i - 1];
+
+            while (j > 0 && combined.charAt(i) != combined.charAt(j)) {
+                j = lps[j - 1];
+            }
+
+            if (combined.charAt(i) == combined.charAt(j)) {
+                j++;
+            }
+
+            lps[i] = j;
+        }
+
+        // Length of longest palindromic prefix
+        int palindromeLength = lps[combined.length() - 1];
+
+        // Remaining suffix
+        String suffix = s.substring(palindromeLength);
+
+        // Reverse suffix and add to front
+        return new StringBuilder(suffix)
+                .reverse()
+                .toString() + s;
+    }
+}
